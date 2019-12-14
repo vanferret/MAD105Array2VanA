5ae187d525a77f0bb7bf457b3bbb0dac320e5dac
AH50977 <vanferret@gmail.com>
2019-12-13 23:21:23 -0600

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,9 @@
+fun main() {
+
+    val tacoSupplies = arrayOf("Tortillas","Lettuce", "Tomatoes", "Cheese", "Onions", "Cilantro","Limes")
+
+    //that's all folks
+    for (i in tacoSupplies){
+        println(i)
+    }
+}
